0f239b407f838486d45063c02ec0f948564625eb
Fabian Frank Werner <fabian@fabianfrankwerner.com>
2025-05-15 09:52:58 +0200

    prepare index.html for step 6

--- the-odin-project/restaurant-page/src/index.js
+++ the-odin-project/restaurant-page/src/index.js
@@ -1,3 +1,12 @@
 import "./index.css";
 
+{/* <h1>Welcome to La Bella Cucina</h1>
+      <img src="../public/pizza.png" alt="Delicious Italian Food" width="600" />
+      <p>
+        At La Bella Cucina, we bring you the heart of Italy on every plate.
+        Enjoy our cozy atmosphere, homemade pasta, fresh ingredients, and a wine
+        list that will make your evening unforgettable. Whether it's a romantic
+        dinner or a family feast, you're always welcome at our table.
+      </p> */}
+
 console.log("Hello, Restaurant!");
