220c55f2486efce377a0dfbc36c991d7b6942b5f
Fabian Frank Werner <fabian@fabianfrankwerner.com>
2025-05-16 22:41:48 +0200

    complete step 6 & 7

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
 # Hello, World!
 
 I make [YouTube](https://youtube.com/@fabianfrankwerner) videos and write a [Newsletter](https://fabianfrankwerner.com/newsletter)!
-
-and I have an exam today in ethical AI hacking. wish me luck
--- the-odin-project/restaurant-page/src/index.js
+++ the-odin-project/restaurant-page/src/index.js
@@ -1,12 +1,14 @@
 import "./index.css";
+import loadHome from "./loadHome";
+import loadMenu from "./loadMenu";
+import loadAbout from "./loadAbout";
 
-{/* <h1>Welcome to La Bella Cucina</h1>
-      <img src="../public/pizza.png" alt="Delicious Italian Food" width="600" />
-      <p>
-        At La Bella Cucina, we bring you the heart of Italy on every plate.
-        Enjoy our cozy atmosphere, homemade pasta, fresh ingredients, and a wine
-        list that will make your evening unforgettable. Whether it's a romantic
-        dinner or a family feast, you're always welcome at our table.
-      </p> */}
+loadHome();
 
-console.log("Hello, Restaurant!");
+const home = document.querySelector("#home");
+const menu = document.querySelector("#menu");
+const about = document.querySelector("#about");
+
+home.addEventListener("click", () => loadHome());
+menu.addEventListener("click", () => loadMenu());
+about.addEventListener("click", () => loadAbout());
