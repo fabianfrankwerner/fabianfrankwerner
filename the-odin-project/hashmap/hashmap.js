b416c71a086e46b96867b38d1b163e5e5b16f911
Fabian Frank Werner <fabian.werner.1@study.thws.de>
2025-06-19 09:16:54 +0200

    time for hashing

---
+++ the-odin-project/hashmap/hashmap.js
@@ -0,0 +1,21 @@
+// if (index < 0 || index >= buckets.length) {
+//     throw new Error("Trying to access index out of bounds");
+//   }
+
+class HashMap {
+  constructor(loadFactor, capacity) {
+    this.loadFactor = loadFactor;
+    this.capacity = capacity;
+  }
+
+  hash(key) {
+    let hashCode = 0;
+
+    const primeNumber = 31;
+    for (let i = 0; i < key.length; i++) {
+      hashCode = primeNumber * hashCode + key.charCodeAt(i);
+    }
+
+    return hashCode;
+  }
+}
