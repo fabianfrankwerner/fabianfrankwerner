3c8b1e66befd0840c7e951f7c18fff5ff79d7d63
Fabian Frank Werner <fabian.werner.1@study.thws.de>
2025-07-22 13:05:07 +0200

    preparations

--- evervault/index.js
+++ evervault/index.js
@@ -4,20 +4,19 @@ const randomChar = () => chars[Math.floor(Math.random() * (chars.length - 1))];
 const randomString = (length) =>
   Array.from(Array(length)).map(randomChar).join("");
 
-const card = document.querySelector(".card"),
-  letters = card.querySelector(".card-letters");
+const card = document.querySelector(".card");
+const letters = card.querySelector(".card--letters");
 
-const handleOnMove = (e) => {
-  const rect = card.getBoundingClientRect(),
-    x = e.clientX - rect.left,
-    y = e.clientY - rect.top;
+function handleOnMove(e) {
+  const rect = card.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
 
   letters.style.setProperty("--x", `${x}px`);
   letters.style.setProperty("--y", `${y}px`);
 
   letters.innerText = randomString(2000);
-};
+}
 
-card.onmousemove = (e) => handleOnMove(e);
-
-card.ontouchmove = (e) => handleOnMove(e.touches[0]);
+card.addEventListener("mousemove", handleOnMove);
+card.addEventListener("touchmove", (e) => handleOnMove(e.touches[0]));
