7435d4e01904ffe508b9d112dc63001a714dafbe
Fabian Frank Werner <fabian.werner.1@study.thws.de>
2025-07-29 14:46:01 +0200

    add link component

--- the-odin-project/shopping-cart/src/App.jsx
+++ the-odin-project/shopping-cart/src/App.jsx
@@ -1,12 +1,14 @@
+import { Link } from "react-router-dom";
+
 const App = () => {
   return (
     <div>
-      <h1>Home page!</h1>
+      <h1>Hello from the main page of the app!</h1>
       <p>Here are some examples of links to other pages</p>
       <nav>
         <ul>
           <li>
-            <a href="store">Shop now</a>
+            <Link to="store">Profile page</Link>
           </li>
         </ul>
       </nav>
