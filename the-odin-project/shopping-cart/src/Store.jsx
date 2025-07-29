3a904da497faf5a50fc2522c26002b51dae2dd89
Fabian Frank Werner <fabian.werner.1@study.thws.de>
2025-07-29 15:18:20 +0200

    setup vegetables array

--- the-odin-project/shopping-cart/src/Store.jsx
+++ the-odin-project/shopping-cart/src/Store.jsx
@@ -1,8 +1,15 @@
+import vegetables from "./vegetables";
+
 const Store = () => {
   return (
     <div>
       <h1>Store</h1>
       <p>Get goin!</p>
+      <ul>
+        {vegetables.map((veg, idx) => (
+          <li key={idx}>{veg.emoji}</li>
+        ))}
+      </ul>
     </div>
   );
 };
