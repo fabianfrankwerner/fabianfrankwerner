518ecde420b94472dad73ee8caba3cad38d20280
Fabian Frank Werner <fabian.werner.1@study.thws.de>
2025-08-04 20:33:53 +0200

    display index

--- the-odin-project/basic-informational-site/index.js
+++ the-odin-project/basic-informational-site/index.js
@@ -1,12 +1,18 @@
+const fs = require("fs");
 const http = require("node:http");
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "application/json" });
-  res.end(
-    JSON.stringify({
-      data: "Hello, World!",
-    })
-  );
+  if (req.url === "/") {
+    fs.readFile(__dirname + "/index.html", (err, data) => {
+      if (err) {
+        res.writeHead(500);
+        res.end("Server error");
+      } else {
+        res.writeHead(200, { "Content-Type": "text/html" });
+        res.end(data);
+      }
+    });
+  }
 });
 
 server.listen(8080);
