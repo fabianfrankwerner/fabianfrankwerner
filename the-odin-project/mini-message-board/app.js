224f90f9172cfbd419638cd9fe8455b982b3e353
Fabian Frank Werner <fabian.werner.1@study.thws.de>
2025-08-13 21:22:12 +0200

    Setup and prepare EJS

--- the-odin-project/mini-message-board/app.js
+++ the-odin-project/mini-message-board/app.js
@@ -1,7 +1,11 @@
+const path = require("node:path");
 const express = require("express");
 const app = express();
 const messageRouter = require("./routes/messageRouter");
 const indexRouter = require("./routes/indexRouter");
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 app.use("/new", messageRouter);
 app.use("/", indexRouter);
