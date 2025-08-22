581bbe416be6dc11acbd58d0418c892b62bfe21f
Fabian Frank Werner <fabian.werner.1@study.thws.de>
2025-08-22 19:46:41 +0200

    Setup bcrypt

--- the-odin-project/members-only/controllers/indexController.js
+++ the-odin-project/members-only/controllers/indexController.js
@@ -1,39 +1,26 @@
 const db = require("../db/queries");
-
-
-
-// async function getUsernames(req, res) {
-//   const usernames = await db.getAllUsernames();
-//   console.log("Usernames: ", usernames);
-//   res.send("Usernames: " + usernames.map((user) => user.username).join(", "));
-// }
-
+// secure the passwords with bcrypt
+const bcrypt = require("bcryptjs");
+// sanitize and validate the form fields
+// validate confirmPassword field using a custom validator.
 
 async function signUpGet(req, res) {
   res.render("sign-up-form", { title: "Sign-Up Form" });
 }
 
-async function signUpPost(req, res) {}
-
-// async function createUsernamePost(req, res) {
-//   const { username } = req.body;
-//   await db.insertUsername(username);
-//   res.redirect("/");
-// }
-
-// app.post("/sign-up", async (req, res, next) => {
-//     try {
-//       const hashedPassword = await bcrypt.hash(req.body.password, 10);
-//       await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
-//         req.body.username,
-//         hashedPassword,
-//       ]);
-//       res.redirect("/");
-//     } catch (error) {
-//       console.error(error);
-//       next(error);
-//     }
-//   });
+async function signUpPost(req, res, next) {
+  //   try {
+  //     const hashedPassword = await bcrypt.hash(req.body.password, 10);
+  //     await pool.query("INSERT INTO users (username, password) VALUES ($1, $2)", [
+  //       req.body.username,
+  //       hashedPassword,
+  //     ]);
+  //     res.redirect("/");
+  //   } catch (error) {
+  //     console.error(error);
+  //     next(error);
+  //   }
+}
 
 module.exports = {
   signUpGet,
