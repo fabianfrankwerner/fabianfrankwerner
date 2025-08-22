6230caa73fe97bf87d6a351322eee56822131c74
Fabian Frank Werner <fabian.werner.1@study.thws.de>
2025-08-22 19:04:19 +0200

    Refactor pool into db folder

--- the-odin-project/members-only/app.js
+++ the-odin-project/members-only/app.js
@@ -1,17 +1,8 @@
 const path = require("node:path");
-const { Pool } = require("pg");
 const express = require("express");
 const session = require("express-session");
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
-
-const pool = new Pool({
-  host: "localhost",
-  user: "fabianfrankwerner",
-  database: "members_only",
-  password: "",
-  port: 5432,
-});
 
 const app = express();
 app.set("views", path.join(__dirname, "views"));
