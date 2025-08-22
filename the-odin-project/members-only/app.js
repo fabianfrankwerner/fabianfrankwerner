18d390652bba1733059e10f0ef427abcac4b5c35
Fabian Frank Werner <fabian.werner.1@study.thws.de>
2025-08-22 19:14:43 +0200

    Setup project skeleton

--- the-odin-project/members-only/app.js
+++ the-odin-project/members-only/app.js
@@ -1,22 +1,24 @@
 const path = require("node:path");
 const express = require("express");
-const session = require("express-session");
-const passport = require("passport");
-const LocalStrategy = require("passport-local").Strategy;
+const indexRouter = require("./routes/indexRouter");
+
+// const passport = require("passport");
+// const session = require("express-session");
+// const LocalStrategy = require("passport-local").Strategy;
 
 const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
-app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
-app.use(passport.session());
-app.use(express.urlencoded({ extended: false }));
+// app.use(session({ secret: "cats", resave: false, saveUninitialized: false }));
+// app.use(express.urlencoded({ extended: false }));
+// app.use(passport.session());
 
-app.get("/", (req, res) => res.render("index"));
+app.use("/", indexRouter);
 
 app.listen(3000, (error) => {
   if (error) {
     throw error;
   }
-  console.log("app listening on port 3000!");
+  console.log(`Listening on port http://localhost:3000`);
 });
