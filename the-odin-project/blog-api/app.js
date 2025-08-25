dc1917a8073f66db66d9216fb69045d31ff9114f
Fabian Frank Werner <fabian.werner.1@study.thws.de>
2025-08-25 20:09:06 +0200

    Setup environment variables

--- the-odin-project/blog-api/app.js
+++ the-odin-project/blog-api/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const dotenv = require("dotenv");
 const app = express();
+dotenv.config();
 
 app.get("/", (req, res) => res.send("Hello, world!"));
 
-const PORT = 3000;
+const PORT = process.env.PORT;
 app.listen(PORT, (error) => {
   if (error) {
     throw error;
