34122a26ae75ab30f3afc3a9d6e43bee2d822167
Fabian Frank Werner <fabian.werner.1@study.thws.de>
2025-08-26 23:31:20 +0200

    Complete seed file

--- the-odin-project/photo-tagging-app/backend/src/seed.js
+++ the-odin-project/photo-tagging-app/backend/src/seed.js
@@ -4,42 +4,56 @@ const prisma = new PrismaClient();
 async function main() {
   const level = await prisma.level.create({
     data: {
-      title: "Convention Level",
+      title: "PXL CON",
       imageUrl:
         "https://preview.redd.it/17j8zvnwqw211.jpg?auto=webp&s=253f7b7151ff615b3ac353e6351f7cffcc4cd126",
       naturalWidth: 3600,
       naturalHeight: 2544,
-      targets: {
-        create: [
-          // you can create characters separately and reference them; this is simplest
-        ],
-      },
     },
   });
 
   const waldo = await prisma.character.create({
     data: { name: "Waldo", slug: "waldo" },
   });
-  const wizard = await prisma.character.create({
-    data: { name: "Wizard", slug: "wizard" },
+  const pacman = await prisma.character.create({
+    data: { name: "Pacman", slug: "pacman" },
+  });
+  const finn = await prisma.character.create({
+    data: { name: "Finn", slug: "finn" },
   });
 
-  // 2150 / 600
-
-  // Example target: a 50x50 px box at (left: 400, top: 200)
   await prisma.target.create({
     data: {
       levelId: level.id,
       characterId: waldo.id,
-      xMin: 400 / level.naturalWidth,
-      yMin: 200 / level.naturalHeight,
-      xMax: (400 + 50) / level.naturalWidth,
-      yMax: (200 + 50) / level.naturalHeight,
+      xMin: 2150 / level.naturalWidth,
+      yMin: 600 / level.naturalHeight,
+      xMax: (2150 + 50) / level.naturalWidth,
+      yMax: (600 + 50) / level.naturalHeight,
+    },
+  });
+  await prisma.target.create({
+    data: {
+      levelId: level.id,
+      characterId: pacman.id,
+      xMin: 2700 / level.naturalWidth,
+      yMin: 2060 / level.naturalHeight,
+      xMax: (2700 + 50) / level.naturalWidth,
+      yMax: (2060 + 50) / level.naturalHeight,
+    },
+  });
+  await prisma.target.create({
+    data: {
+      levelId: level.id,
+      characterId: finn.id,
+      xMin: 375 / level.naturalWidth,
+      yMin: 2425 / level.naturalHeight,
+      xMax: (375 + 50) / level.naturalWidth,
+      yMax: (2425 + 50) / level.naturalHeight,
     },
   });
 
-  // ... create other targets similarly
-  console.log("Seed done");
+  console.log("✅");
 }
 
 main()
