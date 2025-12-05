d7463a3fc56aed35e89ef763e34daa01f9790a66
Fabian Frank Werner <fabian.werner.1@study.thws.de>
2025-12-05 18:43:34 +0100

    Complete Animations

--- 539/animations/src/scenes/oneSeven.tsx
+++ 539/animations/src/scenes/oneSeven.tsx
@@ -25,34 +25,13 @@ export default makeScene2D(function* (view) {
       ref={code}
       fontSize={108}
       fontFamily={"Space Mono"}
-      code={`.search-container:hover {}`}
-    />
-  );
-
-  yield* code().code(
-    `.search-container:hover {
-  background-color: var(--bg-element-hover);
-  box-shadow: 0 1px 6px 0 rgba(23, 23, 23, 0.5);
-}`,
-    2
-  );
-
-  yield* code().selection(code().findFirstRange("hover"), 2);
-
-  yield* code().code(``, 2);
-
-  view.add(
-    <Code
-      ref={code}
-      fontSize={108}
-      fontFamily={"Space Mono"}
       code={`h-[46px]`}
     />
   );
 
   yield* waitFor(2)
 
-  yield* code().code.append('\n\nshadow-[0_1px_6px_0_rgba(23,23,23,0.5)]', 2);
+  yield* code().code(`.h-\[46px\] { height: 46px; }`, 2);
 
   yield* waitFor(2)
 });
