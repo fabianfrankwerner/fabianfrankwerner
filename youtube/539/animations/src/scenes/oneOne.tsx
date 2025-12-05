2498adcd96e071e90544874e4ce3f2b0a0c56373
Fabian Frank Werner <fabian.werner.1@study.thws.de>
2025-12-05 16:12:14 +0100

    Start an animation

---
+++ youtube/539/animations/src/scenes/oneOne.tsx
@@ -0,0 +1,35 @@
+import { makeScene2D, Code, LezerHighlighter } from "@motion-canvas/2d";
+import { all, createRef, DEFAULT, waitFor } from "@motion-canvas/core";
+
+// import {parser} from '@lezer/javascript';
+import {parser} from '@lezer/markdown';
+// import {parser} from '@lezer/json';
+// import {parser} from '@lezer/html';
+
+// const JavaScript = new LezerHighlighter(parser);
+const MD = new LezerHighlighter(parser);
+// const JSON = new LezerHighlighter(parser);
+// const HTML = new LezerHighlighter(parser);
+
+export default makeScene2D(function* (view) {
+  const code = createRef<Code>();
+
+  view.add(
+    <Code
+      highlighter={MD}
+      ref={code}
+      fontSize={108}
+      fontFamily={"Space Mono"}
+      code={""}
+    />
+  );
+  yield* waitFor(1);
+
+  // append immediately
+  code().code.append(`touch style.css`);
+
+  // animate using the signal signature
+  yield* code().code.append('\ntouch index.html', 1);
+
+  yield* waitFor(1);
+});
