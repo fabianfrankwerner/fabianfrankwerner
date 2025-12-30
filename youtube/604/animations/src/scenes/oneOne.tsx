b004e6652b230376319040b19fbf34e46b451e89
Fabian Frank Werner <fabian.werner.1@study.thws.de>
2025-12-30 23:10:21 +0100

    Write oneOne

--- youtube/604/animations/src/scenes/oneOne.tsx
+++ youtube/604/animations/src/scenes/oneOne.tsx
@@ -1,4 +1,4 @@
-import { makeScene2D, Code, LezerHighlighter } from "@motion-canvas/2d";
+import { makeScene2D, Code, LezerHighlighter, lines } from "@motion-canvas/2d";
 import { all, createRef, DEFAULT, waitFor } from "@motion-canvas/core";
 
 export default makeScene2D(function* (view) {
@@ -7,12 +7,67 @@ export default makeScene2D(function* (view) {
   view.add(
     <Code
       ref={code}
-      fontSize={108}
+      fontSize={81}
       fontFamily={"Geist Mono"}
       code={`\
-        def greet name
-  "Hello, #{name}"
-end`}
+# Python
+def analyze_repo(user, repo):
+    if user:
+        print(f"Analyzing {repo}...")
+    else:
+        print("No user provided.")`}
     />
   );
+
+  yield* waitFor(2);
+
+  yield* all(code().selection(lines(2), 2), code().selection(lines(4), 2));
+
+  yield* code().selection(DEFAULT, 2);
+
+  yield* all(code().selection(lines(3), 2), code().selection(lines(5), 2));
+
+  yield* code().selection(DEFAULT, 2);
+
+  yield* waitFor(10);
+
+  yield* code().code(
+    `\
+# Ruby
+def analyze_repo(user, repo)
+  if user
+    puts "Analyzing #{repo}..."
+  else
+    puts "No user provided."
+  end
+end`,
+    4
+  );
+
+  yield* waitFor(2);
+
+  yield* code().selection(code().findAllRanges(/end/gi), 2);
+
+  yield* code().selection(DEFAULT, 2);
+
+  yield* waitFor(2);
+
+  yield* code().code(
+    `\
+# Python
+def greet(name):
+    return f"Hello, {name}"`,
+    4
+  );
+
+  yield* code().code.append(
+    `\n\n\
+# Ruby
+def greet name
+  "Hello, #{name}"
+end`,
+    4
+  );
+
+  yield* waitFor(10);
 });
