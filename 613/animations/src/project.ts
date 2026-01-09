c5535d8225d10e11ee956c71857a0161c44b3915
Fabian Frank Werner <fabian.werner.1@study.thws.de>
2026-01-09 12:43:09 +0100

    animation setup

---
+++ 613/animations/src/project.ts
@@ -0,0 +1,18 @@
+import { makeProject } from "@motion-canvas/core";
+
+import one from "./scenes/one?scene";
+
+import "./global.css";
+
+import { Code, LezerHighlighter } from "@motion-canvas/2d";
+import { parser } from "@lezer/javascript";
+
+Code.defaultHighlighter = new LezerHighlighter(
+  parser.configure({
+    dialect: "ts",
+  })
+);
+
+export default makeProject({
+  scenes: [one],
+});
---
+++ 613/animations/src/scenes/one.tsx
@@ -0,0 +1,68 @@
+import { makeScene2D, Code, LezerHighlighter, lines } from "@motion-canvas/2d";
+import { all, createRef, DEFAULT, waitFor } from "@motion-canvas/core";
+
+export default makeScene2D(function* (view) {
+  const code = createRef<Code>();
+
+  view.add(
+    <Code
+      ref={code}
+      fontSize={81}
+      fontFamily={"Geist Mono"}
+      code={`\
+// bun create elysia app`}
+    />
+  );
+
+  yield* waitFor(1);
+
+yield* code().code.append(`\n\nimport { remarkCodeHike, recmaCodeHike } from "codehike/mdx"
+
+// ...`, 1);
+
+yield* waitFor(1);
+
+yield* code().code.append(`\n\n/** @type {import('codehike/mdx').CodeHikeConfig} */
+const chConfig = {
+  components: { code: "Code" },
+}
+  
+// ...`, 1);
+  
+  yield* waitFor(2);
+
+  yield* code().code(
+    `\
+MDX`,
+    2
+  );
+
+  yield* waitFor(2);
+
+//   yield* code().code.append('\n\n# Poetry?', 1);
+//   yield* code().code.append('\n\n# Pipenv?', 1);
+//   yield* code().code.append('\n\n# Conda?', 1);
+//   yield* code().code.append('\n\n# Rye?', 1);
+
+//   yield* waitFor(8);
+
+//   // 18
+
+//   yield* code().code(
+//     `\
+// # Ruby
+// source 'https://rubygems.org'
+// gem 'sinatra'
+// gem 'httparty'`,
+//     2
+//   );
+
+//   yield* waitFor(4);
+
+//   yield* code().code.append(`\n
+// # ---
+
+// ~ bundle install`, 2);
+
+// yield* waitFor(12);
+});
