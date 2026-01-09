16de8ac664c6030df19e7f93084a8fa6848561be
Fabian Frank Werner <fabian.werner.1@study.thws.de>
2026-01-09 13:20:34 +0100

    Finish Scene One

--- 613/animations/src/scenes/one.tsx
+++ 613/animations/src/scenes/one.tsx
@@ -56,4 +56,47 @@ yield* code().selection(DEFAULT, 1);
 yield* code().code.replace(lines(7), `  .post("/form", ({ body }) => body) // {"hello": "Elysia"}\n`, 1);
 
 yield* waitFor(4);
+
+yield* code().code(
+  `\
+// index.ts
+  
+import { Elysia, t } from 'elysia'
+
+new Elysia()
+    .get('/user/:id', ({ params: { id } }) => id, {
+        params: t.Object({
+            id: t.Number()
+        })
+    })
+    .listen(3000)`,
+  2
+);
+
+yield* waitFor(14);
+
+yield* code().code(
+  `\
+// client.ts
+
+import { treaty } from '@elysiajs/eden'
+import type { App } from './server'
+
+const app = treaty<App>('localhost:3000')
+
+const { data } = await app.user({ id: 617 }).get()
+
+console.log(data)`,
+  2
+);
+
+yield* waitFor(10);
+
+yield* code().code(
+  `\
+bun run dev`,
+  2
+);
+
+yield* waitFor(4);
 });
