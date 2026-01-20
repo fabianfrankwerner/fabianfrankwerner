26199b8791f647bfda402116cc7528f35e543e21
Fabian Frank Werner <fabian.werner.1@study.thws.de>
2026-01-20 17:46:48 +0100

    Refactor bot protection div

--- src/components/signup-form.tsx
+++ src/components/signup-form.tsx
@@ -129,15 +129,13 @@ export function SignupForm({
             />
           </Field>
           <Field>
-            {/* Clerk's CAPTCHA widget for bot protection */}
+            {/* Clerk's CAPTCHA widget for bot protection. */}
             <div
               id="clerk-captcha"
               data-cl-theme="auto"
               data-cl-size="flexible"
               data-cl-language="auto"
             />
-          </Field>
-          <Field>
             <Button type="submit" disabled={isLoading || !isLoaded}>
               {isLoading ? "Signing Up..." : "Sign Up"}
             </Button>
