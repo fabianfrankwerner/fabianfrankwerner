6f644cd531ed9b471134a46796dc817b2f6ddbb9
Fabian Frank Werner <fabian.werner.1@study.thws.de>
2026-01-23 23:08:19 +0100

    feat: sign-out button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,24 @@
 "use client";
 
-import { UserButton } from "@clerk/nextjs";
+import { useClerk } from "@clerk/nextjs";
 import { Authenticated, Unauthenticated } from "convex/react";
 
 import { AuthWrapper } from "@/components/auth-wrapper";
 import LandingPage from "@/components/landing-page";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
+  const { signOut } = useClerk();
+
   return (
     <AuthWrapper>
       <Authenticated>
         <div className="flex flex-col gap-4 p-4">
           <div className="flex items-center justify-between">
             <div className="text-xs">Simultan</div>
-            <UserButton />
+            <Button variant="secondary" onClick={() => signOut()}>
+              Sign Out
+            </Button>
           </div>
         </div>
       </Authenticated>
