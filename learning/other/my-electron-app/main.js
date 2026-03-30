ead54753fb2bde705ed6ff54db6fc06a662afd18
Fabian Frank Werner <fabian@fabianfrankwerner.com>
2026-03-30 22:19:17 +0200

    ipc example

--- learning/other/my-electron-app/main.js
+++ learning/other/my-electron-app/main.js
@@ -1,4 +1,4 @@
-const { app, BrowserWindow } = require('electron/main')
+const { app, BrowserWindow, ipcMain } = require('electron/main')
 
 const path = require('node:path')
 
@@ -15,6 +15,7 @@ const createWindow = () => {
 }
 
 app.whenReady().then(() => {
+  ipcMain.handle('ping', () => 'pong')
   createWindow()
 
   app.on('activate', () => {
