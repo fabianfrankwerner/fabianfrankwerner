3f240cb90822b037a38e7f2d069b63c11e38f56d
Fabian Frank Werner <fabian@fabianfrankwerner.com>
2026-04-05 19:18:47 +0200

    game setup

---
+++ learning/other/the-rust-programming-language/projects/guessing_game/target/debug/deps/guessing_game-a4c99a1a5599b6c4.d
@@ -0,0 +1,5 @@
+/Users/fabianfrankwerner/Documents/GitHub/fabianfrankwerner/learning/other/the-rust-programming-language/projects/guessing_game/target/debug/deps/guessing_game-a4c99a1a5599b6c4.d: src/main.rs
+
+/Users/fabianfrankwerner/Documents/GitHub/fabianfrankwerner/learning/other/the-rust-programming-language/projects/guessing_game/target/debug/deps/guessing_game-a4c99a1a5599b6c4: src/main.rs
+
+src/main.rs:
