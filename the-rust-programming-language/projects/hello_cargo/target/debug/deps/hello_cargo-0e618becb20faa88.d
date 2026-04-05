1fae8f17ef5aac40fc5d04b57ecb89f6156f061e
Fabian Frank Werner <fabian@fabianfrankwerner.com>
2026-04-05 19:16:47 +0200

    cargo exploration

---
+++ the-rust-programming-language/projects/hello_cargo/target/debug/deps/hello_cargo-0e618becb20faa88.d
@@ -0,0 +1,5 @@
+/Users/fabianfrankwerner/Documents/GitHub/fabianfrankwerner/learning/other/the-rust-programming-language/projects/hello_cargo/target/debug/deps/hello_cargo-0e618becb20faa88.d: src/main.rs
+
+/Users/fabianfrankwerner/Documents/GitHub/fabianfrankwerner/learning/other/the-rust-programming-language/projects/hello_cargo/target/debug/deps/hello_cargo-0e618becb20faa88: src/main.rs
+
+src/main.rs:
